0af0b77f7e6fc878053e154d5e85ecfc153b4fe7
unknown <dong0625@korea.ac.kr>
2025-01-16 16:34:25 +0900

    Remove comments

--- model/embedding.py
+++ model/embedding.py
@@ -4,50 +4,6 @@ import torch.nn as nn
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-""" class TokenizerEn(nn.Module):
-    def __init__(self, max_length):
-        super().__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained('distilbert-base-multilingual-cased')
-        self.max_length = max_length
-
-    def forward(self, texts):
-        inputs = self.tokenizer(texts, return_tensors="pt", padding="max_length", truncation=True, max_length=self.max_length)
-        return inputs
-    
-class TokenizerDe(nn.Module):
-    def __init__(self, max_length):
-        super().__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained('distilbert-base-multilingual-cased')
-        self.max_length = max_length
-
-    def forward(self, texts):
-        inputs = self.tokenizer(texts, return_tensors="pt", padding="max_length", truncation=True, max_length=self.max_length)
-        return inputs
-
-class EmbeddingEn(nn.Module):
-    def __init__(self, d_model):
-        super().__init__()
-        self.model = AutoModel.from_pretrained('distilbert-base-multilingual-cased').to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-        d_embed = self.model.config.hidden_size
-        self.linear = nn.Linear(d_embed, d_model)
-
-    def forward(self, inputs):
-        outputs = self.model(**inputs).last_hidden_state
-        out = self.linear(outputs)
-        return out
-    
-class EmbeddingDe(nn.Module):
-    def __init__(self, d_model):
-        super().__init__()
-        self.model = AutoModel.from_pretrained('distilbert-base-multilingual-cased').to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-        d_embed = self.model.config.hidden_size
-        self.linear = nn.Linear(d_embed, d_model)
-
-    def forward(self, inputs):
-        outputs = self.model(**inputs).last_hidden_state
-        out = self.linear(outputs)
-        return out """
-    
 class Tokenizer:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained('distilbert-base-multilingual-cased')
@@ -67,7 +23,7 @@ class Tokenizer:
 class Embedding(nn.Module):
     def __init__(self, d_model):
         super().__init__()
-        self.model = AutoModel.from_pretrained('distilbert-base-multilingual-cased')
+        self.model = AutoModel.from_pretrained('distilbert-base-multilingual-cased').to(DEVICE)
         d_embed = self.model.config.hidden_size
         self.linear = nn.Linear(d_embed, d_model)
 
--- model/layer/multi_head_attention.py
+++ model/layer/multi_head_attention.py
@@ -32,34 +32,3 @@ class MultiHeadAttention(nn.Module):
         out = out.transpose(1, 2).reshape(batch_size, -1, self.h * self.d_v)
         out = self.out_linear(out)
         return out
-
-""" class Attention(nn.Module):
-    def __init__(self, d_model, d_k, d_v):
-        super().__init__()
-        self.d_model = d_model
-        self.d_k = d_k
-        self.Q_linear = nn.Linear(self.d_model, d_k)
-        self.K_linear = nn.Linear(self.d_model, d_k)
-        self.V_linear = nn.Linear(self.d_model, d_v)
-
-    def forward(self, query, key, value, mask = None):
-        # Q, K, V 계산
-        Q = self.Q_linear(query)  # Query: [batch_size, seq_len, d_k]
-        K = self.K_linear(key)  # Key: [batch_size, seq_len, d_k]
-        V = self.V_linear(value)  # Value: [batch_size, seq_len, d_v]
-
-        #내적으로 가중치 구하기
-        weight = torch.matmul(Q, torch.transpose(K, -2, -1)) #L * L
-
-        #scaling
-        weight = weight / torch.sqrt(torch.tensor(self.d_k, dtype=torch.float32))
-
-        #masking
-        if (mask != None):
-            weight.masked_fill_(mask == 0, -1e9)
-
-        #weight sum
-        weight = weight.softmax(dim=-1)
-        out = torch.matmul(weight, V) #L * d_v
-
-        return out """
